3775dbfb08dcc72dd1657d587e30c4210374c0be
Jhon Calsina <jhoncalsina51@gmail.com>
2023-07-24 23:48:16 -0400

    2.2.2 avance oficial

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,17 @@
+// Almacenar elementos del DOM en variables para evitar consultas repetidas
 const spans = document.querySelectorAll('.name span');
+const slider = document.getElementById('slider');
+const toggle = document.getElementById('toggle');
+const flagsElement = document.getElementById("flags");
+const textsToChange = document.querySelectorAll("[data-section]");
+const targets = document.querySelectorAll('[data-target]');
+const content = document.querySelectorAll('[data-content]');
+const menu_toggle = document.getElementById('menu-button');
+const navbar_pop = document.querySelector('.navbar-pop');
+const navLinks = document.querySelectorAll('.nav_links');
+
 let index = 0;
-// mostrar nombre en secuencia
+
 function mostrarSiguienteSpan() {
    if (index < spans.length) {
       spans[index].style.opacity = '1';
@@ -9,31 +20,32 @@ function mostrarSiguienteSpan() {
    }
 }
 
+// Iniciar la secuencia de mostrar nombres
 mostrarSiguienteSpan();
 
-// cambio de tema
+// Cambio de tema
 const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-const slider = document.getElementById('slider');
 
-const setTheme = (theme) => {
+function setTheme(theme) {
    document.documentElement.setAttribute('data-theme', theme);
    localStorage.setItem('theme', theme);
 }
 
+// Evento para cambiar el tema cuando se hace clic en el slider
 slider.addEventListener('click', () => {
    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
    setTheme(switchToTheme);
 })
 
+// Establecer el tema almacenado o el tema preferido al cargar la página
 setTheme(localStorage.getItem('theme') || preferedColorScheme);
 
-const toggle = document.getElementById('toggle');
+// Evento para cambiar el icono de luna/sol en el botón de tema
 toggle.addEventListener('click', function(){
    this.classList.toggle('bi-moon-fill')
 })
 
-const targets = document.querySelectorAll('[data-target]')
-const content = document.querySelectorAll('[data-content]')
+// Cambiar contenido al hacer clic en elementos con atributo [data-target]
 targets.forEach(target => {
    target.addEventListener('click', () => {
       content.forEach(c => {
@@ -44,24 +56,20 @@ targets.forEach(target => {
    })
 })
 
-const menu_toggle = document.getElementById('menu-button');
-const navbar_pop = document.querySelector('.navbar-pop');
-// barra de navegacion
+// Barra de navegación - Evento para mostrar/ocultar el menú móvil
 menu_toggle.addEventListener('click', () => {
    menu_toggle.classList.toggle('bi-x-diamond-fill');
    navbar_pop.classList.toggle('active');
 })
 
-document.querySelectorAll('.nav_links').forEach(n => n.addEventListener('click', () => {
+// Cerrar el menú móvil al hacer clic en un enlace
+navLinks.forEach(n => n.addEventListener('click', () => {
    menu_toggle.classList.remove('bi-x-diamond-fill');
    navbar_pop.classList.remove('active');
 }))
 
-// cambio de lenguajes
-const flagsElement = document.getElementById("flags");
-const textsToChange = document.querySelectorAll("[data-section]");
-
-const changeLanguage = async language => {
+// Cambio de lenguajes - Evento para cambiar el idioma al hacer clic en una bandera
+async function changeLanguage(language) {
    const requestJson = await fetch(`/languages/${language}.json`);
    const texts = await requestJson.json();
 
@@ -72,6 +80,7 @@ const changeLanguage = async language => {
       textToChange.innerHTML = texts[section][value]
    }
 }
+
 flagsElement.addEventListener("click", (e) => {
    changeLanguage(e.target.parentElement.dataset.language);
 })
